f28e228e4513c6b8fd76717663675c629649d874
Pawel Lipinski <lipinskijob@gmail.com>
2024-06-21 21:09:40 +0200

    Extending the Symbol Table

--- src/main/java/com/github/lipinskipawel/mlang/compiler/SymbolTable.java
+++ src/main/java/com/github/lipinskipawel/mlang/compiler/SymbolTable.java
@@ -5,11 +5,14 @@ import java.util.Map;
 import java.util.Optional;
 
 import static com.github.lipinskipawel.mlang.compiler.SymbolTable.SymbolScope.GLOBAL_SCOPE;
+import static com.github.lipinskipawel.mlang.compiler.SymbolTable.SymbolScope.LOCAL_SCOPE;
 import static java.util.Objects.requireNonNull;
+import static java.util.Optional.ofNullable;
 
 public class SymbolTable {
     public enum SymbolScope {
-        GLOBAL_SCOPE("GLOBAL");
+        GLOBAL_SCOPE("GLOBAL"),
+        LOCAL_SCOPE("LOCAL");
         private final String scope;
 
         SymbolScope(String scope) {
@@ -20,6 +23,7 @@ public class SymbolTable {
     public record Symbol(String name, SymbolScope scope, int index) {
     }
 
+    private SymbolTable outer;
     private final Map<String, Symbol> store;
     private int numDefinitions;
 
@@ -32,14 +36,25 @@ public class SymbolTable {
         return new SymbolTable(new HashMap<>(), 0);
     }
 
+    public static SymbolTable enclosedSymbolTable(SymbolTable symbolTable) {
+        final var table = symbolTable();
+        table.outer = symbolTable;
+        return table;
+    }
+
     public Symbol define(String identifier) {
-        final var symbol = new Symbol(identifier, GLOBAL_SCOPE, numDefinitions);
+        final var scope = outer == null ? GLOBAL_SCOPE : LOCAL_SCOPE;
+        final var symbol = new Symbol(identifier, scope, numDefinitions);
         store.put(identifier, symbol);
         numDefinitions++;
         return symbol;
     }
 
     public Optional<Symbol> resolve(String identifier) {
-        return Optional.of(store.get(identifier));
+        final var value = store.get(identifier);
+        if (value == null && outer != null) {
+            return resolve(identifier);
+        }
+        return ofNullable(value);
     }
 }
--- src/test/java/com/github/lipinskipawel/mlang/compiler/SymbolTableTest.java
+++ src/test/java/com/github/lipinskipawel/mlang/compiler/SymbolTableTest.java
@@ -1,24 +1,40 @@
 package com.github.lipinskipawel.mlang.compiler;
 
+import com.github.lipinskipawel.mlang.compiler.SymbolTable.Symbol;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static com.github.lipinskipawel.mlang.compiler.SymbolTable.SymbolScope.GLOBAL_SCOPE;
+import static com.github.lipinskipawel.mlang.compiler.SymbolTable.SymbolScope.LOCAL_SCOPE;
+import static com.github.lipinskipawel.mlang.compiler.SymbolTable.enclosedSymbolTable;
 import static com.github.lipinskipawel.mlang.compiler.SymbolTable.symbolTable;
 
 class SymbolTableTest implements WithAssertions {
 
+    private record SymbolTestCase(SymbolTable table, List<Symbol> expectedSymbols) {
+    }
+
     @Test
     void define_symbol() {
-        var symbolTable = symbolTable();
+        var global = symbolTable();
+        var a = global.define("a");
+        var b = global.define("b");
+        assertThat(a).isEqualTo(new Symbol("a", GLOBAL_SCOPE, 0));
+        assertThat(b).isEqualTo(new Symbol("b", GLOBAL_SCOPE, 1));
 
-        var a = symbolTable.define("a");
-        var b = symbolTable.define("b");
+        var firstLocal = enclosedSymbolTable(global);
+        var c = firstLocal.define("c");
+        var d = firstLocal.define("d");
+        assertThat(c).isEqualTo(new Symbol("c", LOCAL_SCOPE, 0));
+        assertThat(d).isEqualTo(new Symbol("d", LOCAL_SCOPE, 1));
 
-        assertThat(a).isEqualTo(new SymbolTable.Symbol("a", GLOBAL_SCOPE, 0));
-        assertThat(b).isEqualTo(new SymbolTable.Symbol("b", GLOBAL_SCOPE, 1));
+        var secondLocal = enclosedSymbolTable(global);
+        var e = secondLocal.define("e");
+        var f = secondLocal.define("f");
+        assertThat(e).isEqualTo(new Symbol("e", LOCAL_SCOPE, 0));
+        assertThat(f).isEqualTo(new Symbol("f", LOCAL_SCOPE, 1));
     }
 
     @Test
@@ -28,15 +44,80 @@ class SymbolTableTest implements WithAssertions {
         symbolTable.define("b");
 
         var expectedList = List.of(
-                new SymbolTable.Symbol("a", GLOBAL_SCOPE, 0),
-                new SymbolTable.Symbol("b", GLOBAL_SCOPE, 1)
+                new Symbol("a", GLOBAL_SCOPE, 0),
+                new Symbol("b", GLOBAL_SCOPE, 1)
         );
 
-        for (var i = 0; i < expectedList.size(); i++) {
-            var expected = expectedList.get(i);
+        for (var expected : expectedList) {
             var actual = symbolTable.resolve(expected.name());
             assertThat(actual).isPresent();
             assertThat(actual.get()).isEqualTo(expected);
+        }
+    }
+
+    @Test
+    void resolve_local() {
+        var global = symbolTable();
+        global.define("a");
+        global.define("b");
+
+        var local = enclosedSymbolTable(global);
+        local.define("c");
+        local.define("d");
+
+        var expectedList = List.of(
+                new Symbol("a", GLOBAL_SCOPE, 0),
+                new Symbol("b", GLOBAL_SCOPE, 1),
+                new Symbol("c", LOCAL_SCOPE, 0),
+                new Symbol("d", LOCAL_SCOPE, 1)
+        );
+
+        for (var expected : expectedList) {
+            var actual = local.resolve(expected.name());
+            assertThat(actual).isPresent();
+            assertThat(actual.get()).isEqualTo(expected);
+        }
+    }
+
+    @Test
+    void resolve_nested_local() {
+        var global = symbolTable();
+        global.define("a");
+        global.define("b");
+
+        var firstLocal = enclosedSymbolTable(global);
+        firstLocal.define("c");
+        firstLocal.define("d");
+
+        var secondLocal = enclosedSymbolTable(firstLocal);
+        secondLocal.define("e");
+        secondLocal.define("f");
+
+        var tests = List.of(
+                new SymbolTestCase(
+                        firstLocal,
+                        List.of(
+                                new Symbol("a", GLOBAL_SCOPE, 0),
+                                new Symbol("b", GLOBAL_SCOPE, 1),
+                                new Symbol("c", LOCAL_SCOPE, 0),
+                                new Symbol("d", LOCAL_SCOPE, 1)
+                        )),
+                new SymbolTestCase(
+                        secondLocal,
+                        List.of(
+                                new Symbol("a", GLOBAL_SCOPE, 0),
+                                new Symbol("b", GLOBAL_SCOPE, 1),
+                                new Symbol("e", LOCAL_SCOPE, 0),
+                                new Symbol("f", LOCAL_SCOPE, 1)
+                        ))
+        );
+
+        for (var test : tests) {
+            for (var symbol : test.expectedSymbols) {
+                var actual = test.table.resolve(symbol.name());
+                assertThat(actual).isPresent();
+                assertThat(actual.get()).isEqualTo(symbol);
+            }
         }
     }
 }
